e9793368eeb9c14a14d0848a557aafb115d417f1
Clément Duveau <clement@duveau.eu>
2022-04-06 17:55:22 +0200

    Optimisation CSS et CLS

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: {
+      '@fullhuman/postcss-purgecss': {
+        content: ['themes/portio-finops/layouts/**/*.html'],
+      },
+      autoprefixer: {}
+    }
+  };  
